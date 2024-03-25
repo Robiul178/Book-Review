7732ba7f8a11ffea762be351c6ca1d2767096713
Robiul Islam <robiulislam.me2022@gmail.com>
2024-03-26 01:37:20 +0600

    first commit

---
+++ src/Componants/ListBooks/ListBooks.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const ListBooks = () => {
+    return (
+        <div>
+            <h2>ListBooks</h2>
+        </div>
+    );
+};
+
+export default ListBooks;
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Componants/Root/Root';
+
+import Home from './Componants/Home/Home';
+import ListBooks from './Componants/ListBooks/ListBooks';
+import PageToRead from './Componants/PageToRead/PageToRead';
+import Blogs from './Componants/Blogs/Blogs';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/listBooks',
+        element: <ListBooks></ListBooks>
+      },
+      {
+        path: '/pageToRead',
+        element: <PageToRead></PageToRead>
+      },
+      {
+        path: '/blogs',
+        element: <Blogs></Blogs>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
